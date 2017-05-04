a14088be1beddb77b1fec9828f579b3842d505cf
Tin Tran <thuutin@gmail.com>
2017-05-04 19:58:02 +0700

    Add Nullable annotation

--- android-reactive-location/src/main/java/pl/charmas/android/reactivelocation/observables/location/LocationUpdatesObservable.java
+++ android-reactive-location/src/main/java/pl/charmas/android/reactivelocation/observables/location/LocationUpdatesObservable.java
@@ -2,6 +2,7 @@ package pl.charmas.android.reactivelocation.observables.location;
 
 import android.content.Context;
 import android.location.Location;
+import android.support.annotation.Nullable;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -21,7 +22,7 @@ public class LocationUpdatesObservable extends BaseLocationObservable<Location> 
     }
 
     private final LocationRequest locationRequest;
-    private UnsubscribableLocationListener listener;
+    @Nullable private UnsubscribableLocationListener listener;
 
     private LocationUpdatesObservable(Context ctx, LocationRequest locationRequest) {
         super(ctx);
